618a928ca34cd2d70317467b96f5883276b8099c
TalonM <talonm@uw.edu>
2020-11-05 00:15:56 -0800

    Problem 2.2 findMidAndBreak() working

--- src/main/java/Problem2/SortList.java
+++ src/main/java/Problem2/SortList.java
@@ -18,11 +18,51 @@ public class SortList {
 
     public static ListNode findMidAndBreak(ListNode head) {
         // homework
+        if (head != null && head.next != null)
+            return findMidAndBreakHelper(head, head);
         return null;
+    }
+
+    private static ListNode findMidAndBreakHelper(ListNode p1, ListNode p2) {
+
+        if (p2.next != null) {
+            p2 = p2.next.next;
+        } else {
+            p2 = null;
+        }
+
+        if (p2 == null || p2.next == null) {
+            ListNode temp;
+            temp = p1.next;
+            p1.next = null;
+            return temp;
+        }
+
+        p1 = p1.next;
+
+        return(findMidAndBreakHelper(p1, p2));
+
     }
 
     public static ListNode mergeLists(ListNode list1, ListNode list2) {
         // homework
+        if (list1 != null && list2 != null) {
+
+            if (list1.val >= list2.val) {
+                list1.next = mergeLists(list1, list2.next);
+                return list1;
+            } else {
+                list2.next = mergeLists(list1.next, list2);
+                return list2;
+            }
+
+        } else if (list1 != null) {
+            list2.next = mergeLists(list1.next, list2);
+            return list2;
+        } else if (list2 != null) {
+            list1.next = mergeLists(list1, list2.next);
+            return list1;
+        }
         return null;
     }
 }
